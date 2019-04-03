fc7963baf256f3586d09ba0d5430f8d6fe411804
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-04-03 13:51:23 -0400

    Exibindo mensagens(erro,sucesso e alerta) na página

--- src/main/java/br/com/tdstecnologia/blog/controller/post/NovoPostController.java
+++ src/main/java/br/com/tdstecnologia/blog/controller/post/NovoPostController.java
@@ -1,5 +1,6 @@
 package br.com.tdstecnologia.blog.controller.post;
 
+import br.com.tdstecnologia.blog.features.jsf.Jsf;
 import br.com.tdstecnologia.blog.model.post.PostBe;
 import br.com.tdstecnologia.blog.model.post.PostVo;
 import java.io.Serializable;
@@ -10,35 +11,40 @@ import javax.inject.Named;
 @Named
 @ViewScoped
 public class NovoPostController implements Serializable {
-
+    
     private PostVo postVo;
     private PostBe postBe;
-
+    
     public NovoPostController() {
     }
-
+    
     @PostConstruct
     private void init() {
         this.postVo = new PostVo();
         this.postBe = new PostBe();
     }
-
+    
     public String flowNovoPost() {
         return "/post/novo-post";
     }
-
+    
     public void salvarPost() {
-        getPostBe().salvarPost(postVo);
+        try {
+            getPostBe().salvarPost(postVo);
+            Jsf.Msg.sucesso("Post salvo...");
+        } catch (Exception e) {
+            Jsf.Msg.erro(e);
+        }
     }
-
+    
     public PostVo getPostVo() {
         return postVo;
     }
-
+    
     public void setPostVo(PostVo postVo) {
         this.postVo = postVo;
     }
-
+    
     private PostBe getPostBe() {
         return postBe;
     }
