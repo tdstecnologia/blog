e217922db72b2546c18cf707fbd59d2005465405
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-04-03 15:33:40 -0400

    Implementação da AbstractVo

--- src/main/java/br/com/tdstecnologia/blog/model/abstracts/AbstractVo.java
+++ src/main/java/br/com/tdstecnologia/blog/model/abstracts/AbstractVo.java
@@ -1,5 +1,22 @@
 package br.com.tdstecnologia.blog.model.abstracts;
 
-public abstract class AbstractVo {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractVo<T> implements Serializable {
+
+    protected List<T> listVo;
+
+    public List<T> getListVo() {
+        if (this.listVo == null) {
+            this.listVo = new ArrayList<>();
+        }
+        return listVo;
+    }
+
+    public void setListVo(List<T> colls) {
+        this.listVo = colls;
+    }
 
 }
