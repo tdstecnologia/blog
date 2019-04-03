2fd85be0ba312ef5908df9e32cc3d75fb3500b9b
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-04-03 10:06:56 -0400

    PostDao - método salvar post

--- src/main/java/br/com/tdstecnologia/blog/model/post/PostBe.java
+++ src/main/java/br/com/tdstecnologia/blog/model/post/PostBe.java
@@ -1,10 +1,23 @@
 package br.com.tdstecnologia.blog.model.post;
 
 public class PostBe {
-    
-    
-    public void salvarPost(PostVo postVo){
-        System.out.println(postVo);
+
+    private PostDao postDao;
+
+    public PostBe() {
+        this.postDao = new PostDao();
     }
-    
+
+    public void salvarPost(PostVo postVo) {
+        getPostDao().salvarPost(postVo);
+    }
+
+    public PostDao getPostDao() {
+        return postDao;
+    }
+
+    public void setPostDao(PostDao postDao) {
+        this.postDao = postDao;
+    }
+
 }
---
+++ src/main/java/br/com/tdstecnologia/blog/model/post/PostDao.java
@@ -0,0 +1,9 @@
+package br.com.tdstecnologia.blog.model.post;
+
+public class PostDao {
+
+    public void salvarPost(PostVo postVo) {
+        System.out.println(postVo);
+    }
+
+}
