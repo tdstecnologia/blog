edbbb8719666342b884a3be661926fa7e5099b4c
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-04-03 13:49:46 -0400

    Sistema de mensagens para JSF

---
+++ src/main/java/br/com/tdstecnologia/blog/features/jsf/Jsf.java
@@ -0,0 +1,52 @@
+package br.com.tdstecnologia.blog.features.jsf;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class Jsf {
+
+    public static class Msg {
+
+        /**
+         * Recupera a mensagem de erro da exception e adciona nas pilha de
+         * mensagens do JSF.
+         *
+         * Exibi no console a pilha de erro
+         *
+         * @param e - Exception lançada.
+         */
+        public static void erro(final Exception e) {
+            erro(e.getMessage());
+            e.printStackTrace();
+        }
+
+        /**
+         * Adiciona a mensagem na pilha de mensagens do JSF com tipo
+         * SEVERITY_INFO
+         *
+         * @param msg - Mensagem para exibida
+         */
+        public static void sucesso(final String msg) {
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null);
+            FacesContext.getCurrentInstance().addMessage(null, fm);
+        }
+
+        /**
+         * Adiciona a mensagem na pilha de mensagens do JSF com tipo
+         * SEVERITY_WARN
+         *
+         * @param msg - Mensagem para exibida
+         */
+        public static void alerta(final String msg) {
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_WARN, msg, null);
+            FacesContext.getCurrentInstance().addMessage(null, fm);
+        }
+
+        private static void erro(final String msg) {
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null);
+            FacesContext.getCurrentInstance().addMessage(null, fm);
+        }
+
+    }
+
+}
