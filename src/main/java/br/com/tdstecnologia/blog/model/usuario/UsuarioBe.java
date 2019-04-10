c84e44b01d30271b8293bf3abe04e8efb4ca6e60
dherkyan <dherkyan@gmail.com>
2019-04-10 04:58:23 -0400

    UsuarioBe

---
+++ src/main/java/br/com/tdstecnologia/blog/model/usuario/UsuarioBe.java
@@ -0,0 +1,45 @@
+package br.com.tdstecnologia.blog.model.usuario;
+
+import br.com.tdstecnologia.blog.features.exceptions.DaoException;
+import br.com.tdstecnologia.blog.model.abstracts.AbstractBe;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class UsuarioBe extends AbstractBe {
+
+    private UsuarioDao usuarioDao;
+
+    public UsuarioBe() {
+    }
+
+    public void salvarUsuario(UsuarioVo usuarioVo) throws Exception {
+        EntityManager em = getManager();
+        EntityTransaction tx = getTransacao();
+        try {
+            this.usuarioDao = new UsuarioDao(em);
+
+            begin(tx);
+            getUsuarioDao().salvarUsuario(usuarioVo);
+            commit(tx);
+        } catch (DaoException e) {
+            rollback(tx);
+            throw e;
+        } catch (Exception e) {
+            rollback(tx);
+            throw e;
+        } finally {
+            close(em);
+        }
+    }
+
+    public UsuarioDao getUsuarioDao() {
+        return usuarioDao;
+    }
+
+    public void setUsuarioDao(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    
+    
+}
