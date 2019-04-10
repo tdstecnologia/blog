5bd5fd47211ffd18e9609dd8e9d1661d2be3b3b3
dherkyan <dherkyan@gmail.com>
2019-04-10 04:48:21 -0400

    AbstractDao

--- src/main/java/br/com/tdstecnologia/blog/model/abstracts/AbstractDao.java
+++ src/main/java/br/com/tdstecnologia/blog/model/abstracts/AbstractDao.java
@@ -1,5 +1,22 @@
 package br.com.tdstecnologia.blog.model.abstracts;
 
+import javax.persistence.EntityManager;
+
 public abstract class AbstractDao {
 
+    private EntityManager em;
+
+    public AbstractDao(EntityManager em) {
+        this.em = em;
+    }
+
+    protected EntityManager getEm() {
+        return em;
+    }
+
+    protected void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+    
 }
