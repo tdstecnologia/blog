05098c68c98bfa43eac9d7a0b53581a867db3a56
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-04-23 14:29:27 -0400

    Carga de dados na inicialização da aplicação

---
+++ src/main/resources/META-INF/sql/dados.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.tb02_usuario (nome,email,senha,ativo,data_criacao,data_ativacao,token_ativacao,data_criacao_token) VALUES 
+('Bob Andrade','bob@gmail.com','123',NULL,NULL,NULL,'18B051765D2564CC6C14303432434437',NULL)
+;
